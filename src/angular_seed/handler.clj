0e5f456240c48b0c01ed524c508c3416d304c2bf
Chris Devine <cdevine@annadaletech.com>
2013-05-07 14:26:19 +0100

    First commit

---
+++ src/angular_seed/handler.clj
@@ -0,0 +1,13 @@
+(ns angular-seed.handler
+  (:use compojure.core)
+  (:require [compojure.handler :as handler]
+            [compojure.route :as route]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (route/files "/" {:root "app"})
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
